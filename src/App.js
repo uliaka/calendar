e324309369fe594be1aa4e6a5f8598efbceb0a2a
uliaka <uliana.gorin95@gmail.com>
2019-11-28 15:44:10 +0200

    added calnendar to react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Calendar from './components/Calendar/MyCalendar.js';
+
+const events = [
+  {
+    title: "swimming",
+    time: '13:00',
+    date: '22-11-2019'
+  },
+  {
+    title: "dancing",
+    time: '13:00',
+    date: '15-11-2019'
+  },
+  {
+    title: "singing",
+    time: '13:00',
+    date: '13-11-2019',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Calendar events={events}/>
     </div>
   );
 }
